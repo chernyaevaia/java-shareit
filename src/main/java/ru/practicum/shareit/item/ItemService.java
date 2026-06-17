089a80247205d6c30f71ea718564bda8a66653c1
Ирина Черняева <iridache@192.168.1.4>
2026-06-17 17:15:10 +0300

    style fix

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -7,10 +7,16 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import java.util.List;
 
 public interface ItemService {
+    
     ItemDto create(Long userId, ItemDto itemDto);
+
     ItemDto update(Long userId, Long itemId, ItemDto itemDto);
+
     ItemDto getById(Long itemId, Long userId);
+
     List<ItemDto> getAllByOwner(Long userId);
+
     List<ItemDto> search(String text);
+
     CommentDto addComment(Long userId, Long itemId, CommentRequestDto dto);
 }
